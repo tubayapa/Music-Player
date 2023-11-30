2c270eef1c7560b309124647f3a82bfe0adb438a
Tuba Yapa <tubayapa@gmail.com>
2023-11-30 13:02:16 +0000

    first commit

---
+++ playlist.js
@@ -0,0 +1,29 @@
+let songs = [
+    {
+        name: "Killer Queen",
+        artist: "Queen",
+        img: "img1",
+        audio: "music1"
+    },
+
+    {
+        name: "We Are the World",
+        artist: "Michael Jackson",
+        img: "img2",
+        audio: "music2"
+    },
+
+    {
+        name: "Yellow",
+        artist: "Coldplay",
+        img: "img3",
+        audio: "music3"
+    },
+
+    {
+        name: "Life on Mars",
+        artist: "David Bowie",
+        img: "img4",
+        audio: "music4"
+    },
+]
